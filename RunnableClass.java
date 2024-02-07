6a96d4077c1cef85660fce7b9c3859fb3b4343c8
LeloKarma <92229998+LeloKarma@users.noreply.github.com>
2024-02-07 19:50:35 +0100

    RunnableClass.java

---
+++ RunnableClass.java
@@ -0,0 +1,15 @@
+package javafxGui;
+
+public class RunnableClass {
+	  public static void main(String[] args) {
+		  
+		  //create an object of the Aopples class
+		  Aopples apples = new Aopples();
+		  
+		  //call the method using the instance or object created
+		  apples.simpleMethod();
+		  
+		  
+	  }
+
+}
